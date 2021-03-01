582b67ea41cff97ee9bf31044efcda595c9d4651
Aniket762 <aniketindian8@gmail.com>
2021-03-02 02:28:28 +0530

    About init

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import './about.css';
 const About = () => {
   return (
-    <div><h1>About</h1></div>
+    <div>
+      <h1 className="head-1">Now a whole page just about me| Me talking about me</h1>
+      <div className="box-left" >
+        <img src="https://drscdn.500px.org/photo/230800449/m%3D900/v2?sig=2f3a88b5ca95e3e27d9635d98c49936b21af36d65c62d6c411f645f6677fdd2b" className="main-image" />
+        <p>Hey there</p>
+      </div> 
+      <div className="box-right" >
+        <img src="https://drscdn.500px.org/photo/230800449/m%3D900/v2?sig=2f3a88b5ca95e3e27d9635d98c49936b21af36d65c62d6c411f645f6677fdd2b" className="main-image" />
+        <p>Hey there</p>
+      </div> 
+      <div className="box-left" >
+        <img src="https://drscdn.500px.org/photo/230800449/m%3D900/v2?sig=2f3a88b5ca95e3e27d9635d98c49936b21af36d65c62d6c411f645f6677fdd2b" className="main-image" />
+        <p>Hey there</p>
+      </div> 
+    </div>
   );
 };
+
+/* 1. image left a bit about you
+2. image about your passion and career 
+3. extra caricullar */
 
 export default About;
